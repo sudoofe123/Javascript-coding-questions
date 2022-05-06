589423ba4c207c40232c78b7629076fbaa914d3b
Sudhanshu kumar <sudhanshukumar9211@gmail.com>
2022-05-06 23:58:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -142,6 +142,20 @@ for (let i = 0; i < arr.length; i++) {
 const str2 = arr.join(" ");
 console.log(str2);//I Am Learning To Code
 
+//10.simple example of callback function
+
+function modifyArray(arr, callback) {
+  // do something to arr here
+  arr.push(100);
+  // then execute the callback function that was passed
+  callback();
+}
+
+var arr = [1, 2, 3, 4, 5];
+
+modifyArray(arr, function() {
+  console.log("array has been modified", arr);
+});
 
 
 
